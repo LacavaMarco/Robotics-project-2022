570e6d7a0ac5c67de8e2eb783c1a01e0aef77267
Lorenzo <lorenzo.aicardi@mail.polimi.it>
2022-04-20 17:26:51 +0200

    Finished odometry and added broadcaster

--- src/project1/src/odometry.cpp
+++ src/project1/src/odometry.cpp
@@ -1,78 +1,114 @@
 #include "ros/ros.h"
+#include <array>
 
 class Odometry{
 
 public:
-  Odometry() { //variables in which I have to save initialpose
-    this->prev_t = 0;
-    sub_v = n.subscribe("/cmd_vel", 1000, &Odometry::odometryCallback, this);
-    sub_p = n.subscribe("/robot/pose", 1000, &Odometry::odometryCallback, this);
-    pub = n.advertise<nav_msgs::Odometry>("odom", 1000);
-  }
-
-  void odometryCallback(const geometry_msgs::TwistStamped::ConstPtr& msg_v,
-                        const geometry_msgs::PoseStamped::ConstPtr& msg_p) {
-    //get dei parametri della pose attuale,
-    //che ho settato con l'odometryCallback di prima.
-    int lin_vel[3]= msg_v->twist.linear;
-    int ang_vel[3]= msg_v->twist.angular;
-    auto odom_msg = nav_msgs::Odometry;
-    odom_msg.header.frame_id = "";
-    odom_msg.header.stamp = ros::Time::now();
-
-    //If the current time is not the first (so if it is not 0), I compute the
-    //odometry. If it is the first then I do nothing and I store the value in
-    //a prev_t variable, which contains the previous time and is updated at
-    //the end of every cycle.
-    if(msg_p->header.stamp != 0){
-      odom_msg.pose.pose = computeEulerOdometry(msg_v->twist.linear, msg_v->twist.angular,
-                                        msg_p->header.stamp,
-                                        this->prev_t,
-                                        msg_p->pose.position, msg_p->pose.orientation);
-      pub.publish(odom_msg);
+    Odometry() { //variables in which I have to save initialpose
+        this->prev_t = 0;
+        this->firstTime = false;
+        sub_v = n.subscribe("/cmd_vel", 1000, &Odometry::odometryCallback, this);
+        sub_p = n.subscribe("/robot/pose", 1000, &Odometry::odometryCallback, this);
+        pub = n.advertise<nav_msgs::Odometry>("/odom", 1000);
     }
 
-    this->prev_t = msg_p->header.stamp;
+    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg_p){
+        if(!firstTime){
+            currx = msg_p->pose.position.x;
+            curry = msg_p->pose.position.y;
 
-  }
+            tf::Quaternion q{
+                msg_p->pose.orientation.x;
+                msg_p->pose.orientation.y;
+                msg_p->pose.orientation.z;
+                msg_p->pose.orientation.w;
+            }
+            tf::Matrix3x3 m(q);
+            double roll, pitch, yaw;
+            m.getRPY(roll, pitch, yaw);
 
-  //Basic Euler odometry. Needs to be updated once I learn how to use quaternions.
-  geometry_msgs::Pose computeEulerOdometry(geometry_msgs::Vector3 x,
-                                      geometry_msgs::Vector3 y,
-                                      time timestamp,
-                                      time prev_t,
-                                      geometry_msgs::Point initPose
-                                      geometry_msgs::Quaternion initOrientation){
+            currTheta = yaw;
+            firstTime = true;
+        }
+    }
 
-      auto odom_pos = geometry_msgs::Point();
+    void odometryCallback(const geometry_msgs::TwistStamped::ConstPtr& msg_v) {
 
-      t = timestamp - prev_t; //sampling period t
+        //get dei parametri della pose attuale,
+        //che ho settato con l'odometryCallback di prima.
+        auto odom_msg = nav_msgs::Odometry;
+        odom_msg.header.frame_id = "";
+        odom_msg.header.stamp = ros::Time::now();
 
-      odom_pos.point.x =  initPose.x + x*t*3.14; //instead of 3.14 there should be cos(Theta)
-      odom_pos.point.y =  initPose.y + y*t*3.14; //instead of 3.14 there should be sin(Theta)
-      odom_pos.point.z = 0; //it's alwyas 0, it moves on a plane
+        //If the current time is not the first (so if it is not 0), I compute the
+        //odometry. If it is the first then I do nothing and I store the value in
+        //a prev_t variable, which contains the previous time and is updated at
+        //the end of every cycle.
 
-      //needs to be theta as well here, but it's in quaternions (still need to
-      // figure it out)
-  }
-
-
-private:
-  ros::NodeHandle n;
-  nav_msgs::Odometry robot_odometry;
-  ros::Subscriber sub;
-  ros::Publisher pub;
-
-  time prev_t;
+        odom_msg.pose.pose = computeEulerOdometry(msg_v->twist.linear, msg_v->twist.angular,
+            msg_v->header.stamp,
+            this->prev_t,
+            currx, curry, currTheta);
 
 
-}
 
-int main(int argc, char**argv){
-  ros::init(argc, argv, "odometry");
-  Odometry odometry;
-  while(ros::ok){
-    //loop stuff
-  }
-  return 0;
-}
+        odom_msg.twist.twist.linear = msg_v->twist.linear;
+        odom_msg.twist.twist.angular = msg_v->twist.angular;
+
+        pub.publish(odom_msg);
+
+        this->prev_t = msg_p->header.stamp;
+
+    }
+
+        //Basic Euler odometry. Needs to be updated once I learn how to use quaternions.
+        geometry_msgs::Pose computeEulerOdometry(geometry_msgs::Vector3 vel_lin,
+            geometry_msgs::Vector3 vel_ang,
+            time timestamp,
+            time prev_t,
+            float64 x, float64 y, float64 theta){
+
+                auto odom_pos = geometry_msgs::Pose();
+
+                t = timestamp - prev_t; //sampling period t
+
+                odom_pos.point.x =  x + vel_lin.x*t;
+                odom_pos.point.y =  y + vel_lin.y*t;
+                odom_pos.point.z = 0; //it's alwyas 0, it moves on a plane
+
+                currTheta = currTheta * vel_ang.z*t
+
+                tf2::Quaternion q;
+                q.setRPY(0,0, currTheta);
+                odom_pos.orientation.x = q.x();
+                odom_pos.orientation.y = q.y();
+                odom_pos.orientation.z = q.z();
+                odom_pos.orientation.w = q.w();
+
+                currx = odom_pos.point.x;
+                curry = odom_pos.point.y;
+
+                return odom_pos;
+            }
+
+
+        private:
+            ros::NodeHandle n;
+            nav_msgs::Odometry robot_odometry;
+            ros::Subscriber sub;
+            ros::Publisher pub;
+            bool firstTime;
+            time prev_t;
+            float64 currx;
+            float64 curry;
+            float64 currTheta;
+        }
+
+        int main(int argc, char**argv){
+            ros::init(argc, argv, "odometry");
+            Odometry odometry;
+            while(ros::ok){
+                //loop stuff
+            }
+            return 0;
+        }
