97bcdfbc2eb291c342dfc441c7a7779455b89261
Marco <10565431@polimi.it>
2022-04-22 00:43:46 +0200

    Bug fixing, code improvements and implementation of bag tf2broadcast
    Fixed wrong definition in velocity.inverseH0(), changed the assignment of current_time (from ros::Time::now() to the message timestamp) to compute wheels velocities and odometry more accurately, implementation of a tf2 broadcaster for the robot pose retrieved from bag (given by Optitrack) in order to better visualize the differences in trajectory between our estimation and the actual robot

---
+++ src/project1/src/tf2bag.cpp
@@ -0,0 +1,46 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+
+class TfBag {
+
+public:
+    TfBag() {
+        sub = n.subscribe("/robot/pose", 1000, &TfBag::callback, this);
+    }
+
+    void callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+        // set header
+        transformStamped.header.stamp = ros::Time::now();
+        transformStamped.header.frame_id = "odom";
+        transformStamped.child_frame_id = "bag";
+        // set x,y
+        transformStamped.transform.translation.x = msg->pose.position.x;
+        transformStamped.transform.translation.y = msg->pose.position.y;
+        transformStamped.transform.translation.z = 0.0;
+        // set theta
+        transformStamped.transform.rotation.x = msg->pose.orientation.x;
+        transformStamped.transform.rotation.y = msg->pose.orientation.y;
+        transformStamped.transform.rotation.z = msg->pose.orientation.z;
+        transformStamped.transform.rotation.w = msg->pose.orientation.w;
+        // send transform
+        br.sendTransform(transformStamped);
+    }
+
+private:
+    ros::NodeHandle n;
+    tf2_ros::TransformBroadcaster br;
+    geometry_msgs::TransformStamped transformStamped;
+    ros::Subscriber sub;
+};
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "tf_bag");
+    TfBag tf_bag;
+    ros::spin();
+    return 0;
+}
